16ff2779d2d2c4839d3b53c2d0604819f7d60c4a
KittyCatOfMagic27 <47306585+KittyCatOfMagic27@users.noreply.github.com>
2022-03-03 21:44:34 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!--<script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/p5.min.js"></script>-->
+    <!--<script src="https://cdn.jsdelivr.net/npm/p5@1.4.0/lib/addons/p5.sound.min.js"></script>-->
+    <style>
+      
+    </style>
+    <title>Circle Marching</title>
+  </head>
+  <body>
+    <script src="https://cdn.jsdelivr.net/npm/gpu.js@latest/dist/gpu-browser.min.js"></script>
+    <script>
+      const gpu = new GPU();
+    </script>
+    <script src="sketch.js"></script>
+  </body>
+</html>
